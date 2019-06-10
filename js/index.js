d43f818ed83e0a68a12387b8ac51582cd8cf3d2e
jennl97 <jennhott97@gmail.com>
2019-06-09 22:26:09 -0500

    First commit of DOM II project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // Your code goes here
+const button = document.querySelectorAll('.btn');
+button[0].addEventListener('click', event =>{
+    button[0].style.backgroundColor = 'orange';
+});
+button[1].addEventListener('click', event => {
+    button[1].style.backgroundColor = 'green';
+});
+button[2].addEventListener('click', event => {
+    button[2].style.backgroundColor = '#17A2B8';
+});
+
+const introHeadline = document.querySelector('.intro h2');
+// console.log(introHeadline);
+introHeadline.addEventListener('mouseover', event => {
+    introHeadline.textContent = 'We like to party!';
+});
+introHeadline.addEventListener('mouseout', event => {
+    introHeadline.textContent = 'Welcome To Fun Bus!'
+});
+
+const mainNav = document.querySelector('.main-navigation');
+mainNav.addEventListener('mouseenter', event => {
+    mainNav.style.backgroundColor = '#17A2B8';
+    mainNav.style.color = 'white';
+});
+
+window.addEventListener('load', event => {
+    alert ("Hello from the other side");
+});
+
+const footer = document.querySelector('.footer');
+window.addEventListener('scroll', event => {
+    footer.style.backgroundColor = '#17A2B8';
+});
+
+const footerP = document.querySelector('.footer p');
+window.addEventListener('scroll', event =>{
+    footerP.style.color = 'white';
+    footerP.style.fontSize = '24px';
+});
+
+const headerImg = document.querySelector('.intro img');
+headerImg.addEventListener('mouseover', event =>{
+    headerImg.style.display = 'none';
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+// console.log(navLinks);
+navLinks.forEach(item => {
+    item.style.color = 'white';
+    item.addEventListener('click', event =>{
+        event.preventDefault();
+    });
+    });
+
